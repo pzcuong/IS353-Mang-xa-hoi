172931154b33286367cecd16fda1c671434f8f0b
Pham Quoc Cuong <pzcuong@Pham-Quoc-Cuong.local>
2023-05-30 13:30:38 +0700

    write recommend users

--- src/recommend_system/recommend_users.js
+++ src/recommend_system/recommend_users.js
@@ -1,84 +1,70 @@
 const csv = require('csvtojson');
 
-// Preprocess the data
-async function preprocessData(filepath) {
-  try {
-    // Read the CSV file and convert it to JSON
-    const jsonData = await csv().fromFile(filepath);
+class User {
+  constructor(data) {
+    this.userID = data.UserID;
+    this.averageCourseRating = this.handleMissingValue(data.AverageCourseRating, 0);
+    this.pastCourses = this.handleMissingValue(data.PastCourses, '').split(',');
+    this.socialLinks = this.handleMissingValue(data.SocialLinks, '');
+  }
 
-    // Handle missing values and convert data into suitable formats
-    const processedData = jsonData.map((user) => {
-      const processedUser = { ...user };
-      // Handle missing values
-      if (processedUser.AverageCourseRating === 'NULL') {
-        processedUser.AverageCourseRating = 0; // Replace with appropriate default value
-      }
-      if (processedUser.PastCourses === 'NULL') {
-        processedUser.PastCourses = ''; // Replace with appropriate default value
-      }
-      if (processedUser.SocialLinks === 'NULL') {
-        processedUser.SocialLinks = ''; // Replace with appropriate default value
-      }
-      // Split PastCourses into an array
-      processedUser.PastCourses = processedUser.PastCourses.split(',');
-      return processedUser;
-    });
-
-    return processedData;
-  } catch (error) {
-    console.error('Error while reading CSV:', error);
-    return [];
+  handleMissingValue(value, defaultValue) {
+    return value === 'NULL' ? defaultValue : value;
   }
 }
 
-// Calculate similarity between users based on shared courses
-function calculateUserSimilarity(user1, user2) {
-  // Calculate similarity between users based on relevant features (e.g., shared courses)
-  const sharedCourses = [...new Set(user1.PastCourses.filter((course) => user2.PastCourses.includes(course)))];
-  const similarity = sharedCourses.length / Math.sqrt(user1.PastCourses.length * user2.PastCourses.length);
-  return similarity;
-}
-
-// Generate recommendations for a user based on user similarities
-function generateRecommendations(userID, data) {
-  const user = data.find((user) => user.UserID === userID);
-  if (!user) {
-    console.log('User not found!');
-    return [];
+class RecommendationSystem {
+  constructor(filepath) {
+    this.filepath = filepath;
+    this.data = [];
   }
 
-  const recommendations = [];
-  for (const otherUser of data) {
-    if (otherUser.UserID !== user.UserID) {
-      const similarity = calculateUserSimilarity(user, otherUser);
-      if (similarity > 0) {
-        recommendations.push({ UserID: otherUser.UserID, Similarity: similarity });
-      }
+  async loadAndPreprocessData() {
+    try {
+      const jsonData = await csv().fromFile(this.filepath);
+      this.data = jsonData.map((user) => new User(user));
+    } catch (error) {
+      console.error('Error while reading CSV:', error);
     }
   }
 
-  // Sort recommendations by similarity in descending order
-  recommendations.sort((a, b) => b.Similarity - a.Similarity);
+  calculateUserSimilarity(user1, user2) {
+    const sharedCourses = [...new Set(user1.pastCourses.filter((course) => user2.pastCourses.includes(course)))];
+    const similarity = sharedCourses.length / Math.sqrt(user1.pastCourses.length * user2.pastCourses.length);
+    return similarity;
+  }
 
-  return recommendations;
+  generateRecommendations(userID) {
+    const user = this.data.find((user) => user.userID === userID);
+    if (!user) {
+      console.log('User not found!');
+      return [];
+    }
+
+    const recommendations = this.data
+      .filter((otherUser) => otherUser.userID !== user.userID)
+      .map((otherUser) => ({
+        userID: otherUser.userID,
+        similarity: this.calculateUserSimilarity(user, otherUser)
+      }))
+      .filter((recommendation) => recommendation.similarity > 0)
+      .sort((a, b) => b.similarity - a.similarity);
+
+    return recommendations;
+  }
+
+  runRecommendationSystem(userID) {
+    this.loadAndPreprocessData().then(() => {
+      const recommendations = this.generateRecommendations(userID);
+      console.log(`Recommended users for ${userID}:`);
+      recommendations.forEach((recommendation, index) => {
+        console.log(`- Recommendation ${index + 1}: User ${recommendation.userID} - Similarity: ${recommendation.similarity}`);
+      });
+    });
+  }
 }
 
 // Usage example
-async function runRecommendationSystem() {
-  // Preprocess the data
-  const filepath = './train.csv';
-  const processedData = await preprocessData(filepath);
-
-  // Generate recommendations for a user
-  const userID = 'admin';
-  const recommendations = generateRecommendations(userID, processedData);
-
-  // Print the recommendations
-  console.log(`Recommended users for ${userID}:`);
-  recommendations.forEach((recommendation, index) => {
-    console.log(`- Recommendation ${index + 1}: User ${recommendation.UserID} - Similarity: ${recommendation.Similarity}`);
-  });
-}
-
-// Run the recommendation system
-runRecommendationSystem();
+const filepath = './train.csv';
+const recommendationSystem = new RecommendationSystem(filepath);
+recommendationSystem.runRecommendationSystem('STUD001');
